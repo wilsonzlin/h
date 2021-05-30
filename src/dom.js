ab413f928fbb2c5a43a0d6ad0d25bb0218d48f2d
Wilson Lin <code@wilsonl.in>
2021-05-30 21:40:46 +1000

    Fix DOM

--- src/dom.js
+++ src/dom.js
@@ -3,7 +3,8 @@ import {exportTarget, parse} from './_common';
 const createElem = ({elem, props, children}) => {
   const $elem = Object.assign(document.createElement(elem), props);
   for (const c of children) {
-    $elem.append(typeof c == 'string' ? c : createElem(c));
+    // c is either a string or a DOM element (result of calling h).
+    $elem.append(c);
   }
   return $elem;
 }
